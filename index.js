64a9b226c62194e49d3f1a38a5ac6604b7971165
J.D. Bean <jonathandbean@gmail.com>
2017-11-30 18:53:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 function theBeatlesPlay(musicians, instruments) {
-var array = []
-for (var i = 0; i < musicians.length; i++) {
-  array.push ( musicians[i] + " plays " + instruments[i] )
-}
+  var array = []
+  for (var i = 0; i < musicians.length; i++) {
+    array.push ( musicians[i] + " plays " + instruments[i] )
+  }
 return array
 }
 
 function johnLennonFacts(facts) {
-
+  var array = []
+  var i = 0
+  while (facts[i]) {
+    array.push(facts[i] + "!!!")
+    i++
+  }
+  return array
 }
 
 function iLoveTheBeatles(number) {
