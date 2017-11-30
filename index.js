af86daa0cab4f7f2a2f8447135cd83b68c6097a9
J.D. Bean <jonathandbean@gmail.com>
2017-11-30 18:37:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var array = []
 for (var i = 0; i < musicians.length; i++) {
   array.push (musicians[i] + " plays the " + instruments[i] +".")
 }
+return array
 }
 
 function johnLennonFacts(facts) {
